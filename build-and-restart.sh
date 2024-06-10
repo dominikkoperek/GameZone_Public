a9907f3c285f4b527e631419e3c720037256adc2
dominikkoperek <yoda1991@wp.pl>
2024-06-10 12:25:30 +0200

    script fix

--- build-and-restart.sh
+++ build-and-restart.sh
@@ -1,4 +1,4 @@
 docker build -t gamezone .
 docker stop gamezone || true
 docker rm gamezone || true
-docker run -d -p 8080:8080 --name=gamezone gamezone
+docker run -it -p 8080:8080 --name=gamezone gamezone
